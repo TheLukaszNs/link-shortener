a4149d5369f3187882e68be6e27a9f7d1c4b2bc2
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2022-08-22 16:06:44 +0200

    feat(p:Home): initial home design 💄

---
+++ src/components/home/link-input.tsx
@@ -0,0 +1,10 @@
+export const LinkInput = () => {
+  return (
+    <div className="w-1/2 mx-auto shadow-md flex group">
+      <input className="flex-1 rounded-l-lg bg-transparent outline-none text-white px-4 py-4 border border-lime-400 bg-neutral-800" />
+      <button className="rounded-r-lg px-6 py-2 outline-none bg-lime-400 text-white font-bold">
+        shorten it! 🔗
+      </button>
+    </div>
+  );
+};
