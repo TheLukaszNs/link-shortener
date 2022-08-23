b32c33ce27aa857106a9b593fe302dea811173da
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2022-08-24 00:44:16 +0200

    feat(p:Home): redesign Home page ♻️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { LinkInput } from "../components/home/link-input";
+import { LinkInput } from "../components/home/LinkInput";
+import { useDarkMode } from "../contexts/darkModeContext";
 
 const Home: NextPage = () => {
+  const { toggleMode } = useDarkMode();
+
   return (
     <>
       <Head>
@@ -10,11 +13,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-screen bg-neutral-900 min-h-screen flex flex-col justify-center items-center relative">
-        <h1 className="text-white font-bold text-[3rem] select-none mb-16">
-          — shorten your <span className="text-lime-400 ">link</span> —
+      <main className="flex flex-col justify-center items-center h-screen gap-4 w-1/3 mx-auto">
+        <button
+          onClick={toggleMode}
+          className="absolute top-8 left-8 dark:text-white"
+        >
+          Toggle Dark Mode
+        </button>
+        <h1 className="text-[3rem] font-bold mb-8 text-black dark:text-white">
+          shorten your <span className="text-teal-400">link</span>
         </h1>
-
         <LinkInput />
       </main>
     </>
